d30309d19282a608645adc399a30c323e0327837
Matthew Villeneuve <matthew.villeneuve@gmail.com>
2020-10-02 15:56:55 -0700

    fix vim conf ln

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ln -sf ${BASEDIR}/bash_profile ~/.bash_profile
 ln -sf ${BASEDIR}/tmux.conf ~/.tmux.conf
 ln -sf ${BASEDIR}/ripgreprc ~/.ripgreprc
 ln -sf ${BASEDIR}/vim ~/.vim
-ln -sf ${BASEDIR}/vim /vimrc ~/.vimrc
+ln -sf ${BASEDIR}/vim/vimrc ~/.vimrc
 ln -sf ${BASEDIR}/zshrc ~/.zshrc
 
 #Add the gitignore settings dotfile to the local gitconfigs:
