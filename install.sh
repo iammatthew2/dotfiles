205323748c25521f7580d96ff323b20a11f076dc
Matthew Villeneuve <matthew.villeneuve@gmail.com>
2021-01-26 13:02:03 -0800

    check file before creating link

--- install.sh
+++ install.sh
@@ -2,12 +2,39 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+if [[ -e ~/.bash_profile ]];then
+  mv ~/.bash_profile ~/.bash_profile.bak
+fi
 ln -sf ${BASEDIR}/bash_profile ~/.bash_profile
+
+if [[ -e ~/.tmux.conf ]];then
+  mv ~/.tmux.conf ~/.tmux.conf.bak
+fi
 ln -sf ${BASEDIR}/tmux.conf ~/.tmux.conf
+
+if [[ -e ~/.ripgreprc ]];then
+  mv ~/.ripgreprc ~/.ripgreprc.bak
+fi
 ln -sf ${BASEDIR}/ripgreprc ~/.ripgreprc
+
+if [[ -e ~/.vim ]];then
+  mv ~/.vim ~/.vim.bak
+fi
 ln -sf ${BASEDIR}/vim ~/.vim
+
+if [[ -e ~/.vimrc ]];then
+  mv ~/.vimrc ~/.vimrc.bak
+fi
 ln -sf ${BASEDIR}/vim/vimrc ~/.vimrc
+
+if [[ -e ~/.zshrc ]];then
+  mv ~/.zshrc ~/.zshrc.bak
+fi
 ln -sf ${BASEDIR}/zshrc ~/.zshrc
+
+if [[ -e ~/.watchman-processor.config.js ]];then
+  mv ~/.watchman-processor.config.js ~/.watchman-processor.config.js.bak
+fi
 ln -sf ${BASEDIR}/watchman-processor.config.js ~/.watchman-processor.config.js
 
 #Add the gitignore settings dotfile to the local gitconfigs:
