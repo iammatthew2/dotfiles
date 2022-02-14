7d5de1e9f48999d6073e0011db9d311b014cbaa1
Matthew Villeneuve <matthew.villeneuve@gmail.com>
2022-02-14 12:51:38 -0800

    update zsh theme - might break remote

--- install.sh
+++ install.sh
@@ -35,6 +35,11 @@ ln -sf ${BASEDIR}/zshrc ~/.zshrc
 if [[ -e ~/.watchman-processor.config.js ]];then
   mv ~/.watchman-processor.config.js ~/.watchman-processor.config.js.bak
 fi
+
+# handle zsh plugins & themes
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+
 ln -sf ${BASEDIR}/watchman-processor.config.js ~/.watchman-processor.config.js
 
 #Add the gitignore settings dotfile to the local gitconfigs:
