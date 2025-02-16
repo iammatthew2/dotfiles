d7c05a7611aa84856d6bfaa667e7309121e08870
Matthew Villeneuve <matthew.villeneuve@gmail.com>
2025-02-16 13:37:46 -0800

    vim color fix for remote

--- install.sh
+++ install.sh
@@ -27,12 +27,6 @@ if [[ -e ~/.zshrc ]];then
 fi
 ln -sf ${BASEDIR}/zshrc ~/.zshrc
 
-# handle zsh plugins & themes
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
-
-ln -sf ${BASEDIR}/watchman-processor.config.js ~/.watchman-processor.config.js
-
 #Add the gitignore settings dotfile to the local gitconfigs:
 git config --global core.excludesfile ${BASEDIR}/gitignore_global
 
