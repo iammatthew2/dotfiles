b416e685490dc45710dc8817ef8e4981d9f06bb9
Matthew Villeneuve <matthew.villeneuve@gmail.com>
2025-10-05 10:17:39 -0700

    handle raspberry setup

--- zsh_config/prompt/prompt_purification_setup.zsh
+++ zsh_config/prompt/prompt_purification_setup.zsh
@@ -129,11 +129,13 @@ prompt_purification_setup() {
     prompt_git_branch
     RPROMPT='$(prompt_git_info) $(git_prompt_status)'
 
-    # Set prompt color based on OS
+       # Set prompt color based on OS
     if [[ "$(uname)" == "Darwin" ]]; then
         PROMPT='%F{magenta}${PWD##*/}%f %B%F{117}>%f%b '
-    else
+    elif [[ -n "$RDEV_NAME" ]]; then
         PROMPT='%F{green}${RDEV_NAME} %F{117}%~ %F{magenta}$%f '
+    else
+        PROMPT='%F{green}%m %F{117}%~ %F{magenta}$%f '
     fi
 }
 
